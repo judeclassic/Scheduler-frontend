05bcbf0995738246b10911351e7d8c0325141467
judeclassic <justclassic24@gmail.com>
2022-09-25 10:08:00 +0100

    ready to push the first time

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,6 +1,6 @@
 //@ts-check
 export { default as Sidebar } from './siderbar'
-export { default as TopBar } from './topbar'
+export { default as TopBar } from './topbars'
 export { default as HomeTabView } from './home'
 export { default as SettingsTabView } from './settings/accounts-tab'
 export { default as TestingTabView } from './testing'
